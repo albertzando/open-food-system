4e7e25c81fdaa96f4d0dfc64cdad7a7a46d81c34
albertzando <olvralbert@gmail.com>
2026-07-16 11:46:36 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
+fetch("data/equipe.json")
+  .then(response => response.json())
+  .then(equipe => {
 
+    const conteudo = document.getElementById("conteudo");
+
+    equipe.forEach(pessoa => {
+
+      conteudo.innerHTML += `
+        <div style="border:1px solid #ccc; padding:15px; margin:10px; border-radius:8px;">
+          <h2>${pessoa.nome}</h2>
+          <p><strong>Função:</strong> ${pessoa.funcao}</p>
+        </div>
+      `;
+
+    });
+
+  });
